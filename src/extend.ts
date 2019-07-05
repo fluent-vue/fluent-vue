651a37dfb0beb4fc5098b6b5654022f4d2dc1bae
Ivan Demchuk <ivan.demchuk@gmail.com>
2019-07-05 22:26:02 +0300

    Add more type info

--- src/extend.ts
+++ src/extend.ts
@@ -1,4 +1,9 @@
-export default function extend(Vue: any): void {
+import { Vue, VueConstructor } from 'vue/types/vue'
+
+/**
+ * Extend Vue prototype with new methods
+ */
+export default function extend(Vue: VueConstructor<Vue>): void {
   Object.defineProperty(Vue.prototype, '$fluent', {
     get() {
       return this._fluent
--- src/types/vue.d.ts
+++ src/types/vue.d.ts
@@ -1,8 +1,15 @@
 import FluentVue from '../fluent-vue'
+import Vue from 'vue'
 
 declare module 'vue/types/vue' {
   interface Vue {
     $fluent?: FluentVue
     _fluent?: FluentVue
+  }
+}
+
+declare module 'vue/types/options' {
+  interface ComponentOptions<V extends Vue> {
+    fluent?: FluentVue
   }
 }
