1856eeb0e02aff5623b88125f41cbedb7aaa32fd
Ivan Demchuk <ivan.demchuk@gmail.com>
2019-07-05 22:37:44 +0300

    Simplify mixin

--- src/mixin.ts
+++ src/mixin.ts
@@ -1,25 +1,19 @@
+import Vue from 'vue'
 import FluentVue from './fluent-vue'
-import { Vue } from 'vue/types/vue'
 
 export default {
   beforeCreate(this: Vue): void {
     const options: any = this.$options
 
-    if (options.fluent) {
-      if (options.fluent instanceof FluentVue) {
-        this._fluent = options.fluent
-      } else {
-        // TODO: Per component config
-      }
+    if (options.fluent && options.fluent instanceof FluentVue) {
+      this._fluent = options.fluent
     } else if (this.$root && this.$root.$fluent && this.$root.$fluent instanceof FluentVue) {
-      // root i18n
       this._fluent = this.$root.$fluent
     } else if (
       options.parent &&
       options.parent.$fluent &&
       options.parent.$fluent instanceof FluentVue
     ) {
-      // parent i18n
       this._fluent = options.parent.$fluent
     }
   },
@@ -29,9 +23,8 @@ export default {
       return
     }
 
-    const self = this
     this.$nextTick(() => {
-      self._fluent = undefined
+      this._fluent = undefined
     })
   }
 }
