7347651efbbd00a720c222186d4f10e8e49409a4
Ivan Demchuk <ivan.demchuk@gmail.com>
2020-01-20 22:02:04 +0200

    chore(dependencies): update rollup dependencies

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,8 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import camelCase from 'lodash.camelcase'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
 
 const pkg = require('./package.json')
 
@@ -24,7 +23,7 @@ export default {
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript(),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs({
       namedExports: {
@@ -35,9 +34,6 @@ export default {
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
-
-    // Resolve source maps to the original source
-    sourceMaps(),
+    resolve()
   ],
 }
