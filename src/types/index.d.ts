ece0fde7c56ebb8751e053f4d2127d63911ba5cb
Ivan Demchuk <ivan.demchuk@gmail.com>
2020-01-22 09:49:47 +0200

    refactor: move typescript types around

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,19 +1,35 @@
-import { Vue } from 'vue/types/vue'
+import Vue, { PluginFunction } from 'vue'
 import { FluentBundle, MessageInfo, Pattern } from '@fluent/bundle'
+import { FluentVueObject, FluentVueOptions, IUpdatable } from '../interfaces'
 
 declare module 'vue/types/vue' {
   interface Vue {
+    /** @private */
+    _fluent?: FluentVueObject
+
     $fluent: FluentVueObject
-    $t(key: string, values: Record<string, unknown>): string
+
+    $t(key: string, values?: Record<string, unknown>): string
   }
 }
 
 declare module 'vue/types/options' {
   interface ComponentOptions<V extends Vue> {
-    fluent?: FluentVueObject
+    fluent?: FluentVue
   }
 }
 
 declare module '*.ftl' {
   export default String
 }
+
+declare class FluentVue {
+  format(key: string, value?: object): string
+  formatAttrs(key: string, value?: object): Record<string, string>
+
+  constructor(options: FluentVueOptions)
+
+  static install: PluginFunction<never>
+}
+
+export default FluentVue
