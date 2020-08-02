df79c9873dc69feb7abf4193a07d53a62ddbaba9
Ivan Demchuk <ivan.demchuk@gmail.com>
2020-08-02 19:23:16 +0300

    test: add tests for dynamically adding bundles

--- packages/fluent-vue/__tests__/changeLanguage.spec.ts
+++ packages/fluent-vue/__tests__/changeLanguage.spec.ts
@@ -115,6 +115,48 @@ describe('language change', () => {
     expect(mounted.html()).toEqual(`<a href="/foo">link text</a>`)
   })
 
+  it('works when dynamically adding bundles', async () => {
+    // Arrange
+    const localVue = createLocalVue()
+
+    bundleEn = new FluentBundle('en-US')
+
+    const fluent = createFluentVue({
+      locale: 'en-US',
+      bundles: [bundleEn],
+    })
+    localVue.use(fluent)
+
+    bundleEn.addResource(
+      new FluentResource(ftl`
+      link = link text
+      `)
+    )
+
+    bundleUk = new FluentBundle('uk-UA')
+    bundleUk.addResource(
+      new FluentResource(ftl`
+      link = текст посилання
+      `)
+    )
+
+    const component = {
+      template: `<a v-t:link href="/foo">Fallback text</a>`,
+    }
+
+    const mounted = mount(component, { localVue })
+    expect(mounted.html()).toEqual(`<a href="/foo">link text</a>`)
+
+    // Act
+    fluent.bundles = fluent.bundles.concat(bundleUk)
+    fluent.locale = 'uk'
+
+    await Vue.nextTick()
+
+    // Assert
+    expect(mounted.html()).toEqual(`<a href="/foo">текст посилання</a>`)
+  })
+
   it('updates child components with overrides', async () => {
     // Arrange
     const localVue = createLocalVue()
