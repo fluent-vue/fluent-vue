21ecbd74d5eff8a6652d7f790625ec5cc9934af5
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-02-23 10:12:42 +0200

    chore: update vite example

--- examples/vite/vite.config.js
+++ examples/vite/vite.config.js
@@ -1,16 +1,29 @@
-export default {
-  optimizeDeps: {
-    link: ['fluent-vue'],
-  },
-  vueCustomBlockTransforms: {
-    i18n({ code, query }) {
-      return `
+import vue from '@vitejs/plugin-vue'
+import { parse as parseUrl } from 'url'
+
+const fluentPlugin = {
+  name: 'fluent-vue',
+  transform(code, id) {
+    if (!/vue&type=i18n/.test(id)) {
+      return
+    }
+
+    const request = parseUrl(id)
+    const query = new URLSearchParams(request.search)
+
+    return `
 import { FluentResource } from '@fluent/bundle'
 
 export default function (Component) {
   Component.fluent = Component.fluent || {}
-  Component.fluent['${query.locale}'] = new FluentResource(\`${code}\`)
+  Component.fluent['${query.get('locale')}'] = new FluentResource(\`${code}\`)
 }`
-    },
   },
+}
+
+export default {
+  optimizeDeps: {
+    link: ['fluent-vue'],
+  },
+  plugins: [vue(), fluentPlugin],
 }
