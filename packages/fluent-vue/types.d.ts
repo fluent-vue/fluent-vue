f60d9a7dac97f153e96815e46480805094eca704
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-04-17 23:39:28 +0300

    Update type definitions

--- packages/fluent-vue/types.d.ts
+++ packages/fluent-vue/types.d.ts
@@ -17,3 +17,54 @@ declare module 'vue/types/options' {
     fluent?: Record<string, FluentResource>
   }
 }
+
+/**
+ * Creates FluentVue instance that can be used on a Vue app.
+ *
+ */
+export declare function createFluentVue(options: FluentVueOptions): FluentVue
+
+export declare interface FluentVue {
+  /** Currently selected locale */
+  locale: string | string[]
+  /** List of bundles used in application */
+  bundles: FluentBundle[]
+  format(key: string, value?: Record<string, FluentVariable>): string
+  formatAttrs(key: string, value?: Record<string, FluentVariable>): Record<string, string>
+  formatWithAttrs: (
+    key: string,
+    value?: Record<string, FluentVariable> | undefined
+  ) => TranslationWithAttrs
+  install(vue: any): void
+}
+
+export declare interface FluentVueOptions {
+  /** Currently selected locale */
+  locale: string | string[]
+  /** List of bundles used in application */
+  bundles: FluentBundle[]
+}
+
+declare class TranslationContext {
+  format: (key: string, value?: Record<string, FluentVariable> | undefined) => string
+  formatAttrs: (
+    key: string,
+    value?: Record<string, FluentVariable> | undefined
+  ) => Record<string, string>
+  formatWithAttrs: (
+    key: string,
+    value?: Record<string, FluentVariable> | undefined
+  ) => TranslationWithAttrs
+  $t: (key: string, value?: Record<string, FluentVariable> | undefined) => string
+  $ta: (key: string, value?: Record<string, FluentVariable> | undefined) => Record<string, string>
+}
+
+declare interface TranslationWithAttrs {
+  value: string
+  hasValue: boolean
+  attributes: Record<string, string>
+}
+
+export declare function useFluent(): TranslationContext
+
+export {}
