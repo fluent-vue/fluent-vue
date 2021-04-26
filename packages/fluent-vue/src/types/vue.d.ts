78da139586a298bfbe8b50e1b282c5a537f40309
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-04-26 08:48:24 +0300

    Include vue augmentations in fluent-vue types

---
+++ packages/fluent-vue/src/types/vue.d.ts
@@ -0,0 +1,17 @@
+import { FluentResource } from '@fluent/bundle'
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $t(key: string, values?: Record<string, unknown>): string
+    $ta(key: string, values?: Record<string, unknown>): Record<string, string>
+  }
+}
+
+declare module 'vue/types/options' {
+  interface ComponentOptions<V extends Vue> {
+    /**
+     * Message override for Vue component
+     */
+    fluent?: Record<string, FluentResource>
+  }
+}
