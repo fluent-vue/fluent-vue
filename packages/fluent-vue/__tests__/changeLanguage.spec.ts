4ef37c1d62c34b7ddf9b4ad908e49f8eaaa3828c
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-05-29 23:38:14 +0300

    Fix jest tests

--- packages/fluent-vue/__tests__/changeLanguage.spec.ts
+++ packages/fluent-vue/__tests__/changeLanguage.spec.ts
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { nextTick } from 'vue-demi'
 import { FluentBundle, FluentResource } from '@fluent/bundle'
 import ftl from '@fluent/dedent'
--- packages/fluent-vue/__tests__/errorHandling.spec.ts
+++ packages/fluent-vue/__tests__/errorHandling.spec.ts
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { FluentBundle, FluentResource } from '@fluent/bundle'
 import ftl from '@fluent/dedent'
 
