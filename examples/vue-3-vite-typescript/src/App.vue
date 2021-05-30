7c28a3189ade2658d046d27bc4f56ce413d63488
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-05-30 15:07:18 +0000

    Rename examples

---
+++ examples/vue-3-vite-typescript/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h3>Examples</h3>
+
+    <h4>$t method</h4>
+    <div>
+      <span>{{ $t('greeting', { name: 'World' }) }}</span>
+    </div>
+
+    <h4>$ta method</h4>
+    <div>
+      <span v-bind="$ta('greeting')">{{ $t('greeting', { name: 'World' }) }}</span>
+    </div>
+
+    <h4>Directive</h4>
+    <div>
+      <span v-t:greeting="{ name: 'World' }"></span>
+    </div>
+
+    <h4>Component</h4>
+    <div>
+      <i18n path="greeting" tag="div">
+        <template #name>
+          <b>{{ $t('user-name') }}</b>
+        </template>
+      </i18n>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'typescript',
+  computed: {
+    username() {
+      return this.$t('user-name')
+    },
+    greeting() {
+      return this.$ta('greeting')
+    },
+  },
+})
+</script>
+
+<style lang="scss">
+.test {
+  display: block;
+}
+</style>
---
+++ examples/vue-3-vite-typescript/src/index.ts
@@ -0,0 +1,26 @@
+import { createApp } from 'vue'
+import { FluentBundle, FluentResource } from '@fluent/bundle'
+import ftl from '@fluent/dedent'
+import { createFluentVue } from 'fluent-vue'
+
+import App from './App.vue'
+
+const bundle = new FluentBundle('en')
+
+bundle.addResource(new FluentResource('user-name = World'))
+bundle.addResource(new FluentResource('aria-key = Aria value'))
+bundle.addResource(
+  new FluentResource(
+    ftl`
+  greeting = Hello, {$name}
+    .aria-label = Label value
+  `
+  )
+)
+
+const fluent = createFluentVue({
+  locale: 'en',
+  bundles: [bundle],
+})
+
+createApp(App).use(fluent).mount('#root')
