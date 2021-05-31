dc1fc12490d7cc7e98d3475c54c9cd594b85808b
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-05-31 20:28:58 +0300

    Make script output nicer

--- scripts/build-examples.js
+++ scripts/build-examples.js
@@ -1,5 +1,6 @@
-const execa = require('execa')
 const fs = require('fs')
+const execa = require('execa')
+const chalk = require('chalk')
 
 const examples = fs
   .readdirSync('examples')
@@ -18,12 +19,14 @@ async function buildExamples() {
   })
 
   for (const example of examples) {
+    console.log(chalk.bold(chalk.yellow(`building ${example.folder}...`)))
     await execa('yarn', ['add', '../../fluent-vue.tgz'], { stdio: 'inherit', cwd: example.folder })
     await execa('yarn', [], { stdio: 'inherit', cwd: example.folder })
     await execa('yarn', ['build'], { stdio: 'inherit', cwd: example.folder })
   }
 
   for (const typescriptExample of examples.filter((ex) => ex.package.scripts.typecheck != null)) {
+    console.log(chalk.bold(chalk.yellow(`typechecking ${typescriptExample.folder}...`)))
     await execa('yarn', ['typecheck'], { stdio: 'inherit', cwd: typescriptExample.folder })
   }
 }
