cf7a93a636d491d24a23652b9fbc87ba7873ba15
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-06-05 13:35:58 +0300

    Add composition api usage example

--- examples/vue-2-vite-typescript/src/App.vue
+++ examples/vue-2-vite-typescript/src/App.vue
@@ -25,24 +25,45 @@
         </template>
       </i18n>
     </div>
+
+    <h4>Composition api (useFluent)</h4>
+    <div v-bind="compositionGreeting">
+      {{ compositionGreeting }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { useFluent } from 'fluent-vue'
 
 export default Vue.extend({
   name: 'typescript',
+  setup () {
+    const { $t, $ta } = useFluent()
+
+    return {
+      compositionUsername: $t('user-name', { name: $t('user-name') }),
+      compositionGreeting: $ta('greeting')
+    }
+  },
   computed: {
-    username() {
+    username (): string {
       return this.$t('user-name')
     },
-    greeting() {
+    greeting (): string {
       return this.$ta('greeting')
     },
   },
 })
 </script>
+
+<fluent locale="en">
+user-name = World
+aria-key = Aria value
+greeting = Hello, {$name}
+  .aria-label = Label value
+</fluent>
 
 <style>
 .test {
