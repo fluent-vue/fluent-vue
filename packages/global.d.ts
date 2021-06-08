bf158d431c4729ef0bc405784dd94af65f0dfb43
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-06-08 23:24:18 +0300

    Remove not used typings

--- packages/global.d.ts
+++ packages/global.d.ts
@@ -1,10 +1,3 @@
 declare module '@fluent/dedent' {
   export default function ftl (strings: TemplateStringsArray): string
 }
-
-declare module 'cached-iterable' {
-  // eslint-disable-next-line @typescript-eslint/no-extraneous-class
-  export class CachedSyncIterable {
-    static from<T>(array: T[]): Iterable<T>
-  }
-}
