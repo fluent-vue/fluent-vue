5c6acdbd516e994ca653fba1728e58f188442d43
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-06-10 09:22:32 +0300

    Add test about message override

--- packages/fluent-vue/__tests__/vue/messageOverride.spec.ts
+++ packages/fluent-vue/__tests__/vue/messageOverride.spec.ts
@@ -52,4 +52,22 @@ describe('message override', () => {
     // Assert
     expect(mounted.html()).toEqual('<a href="/foo">текст посилання 2</a>')
   })
+
+  it('can override both locales from a bundle with multiple locales', async () => {
+    // Arrange
+    const component = {
+      template: '<a v-t:link href="/foo">Fallback text</a>',
+      fluent: {
+        'uk uk-UA': new FluentResource(ftl`
+        link = текст посилання 2
+        `)
+      }
+    }
+
+    // Act
+    const mounted = mountWithFluent(fluent, component)
+
+    // Assert
+    expect(mounted.html()).toEqual('<a href="/foo">текст посилання 2</a>')
+  })
 })
