3ec4f132f6847f289a2ed5979a68835aa11b84e7
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-06-18 09:47:36 +0300

    Upgrate to vuepress v2

---
+++ docs/.vuepress/clientAppEnhance.ts
@@ -0,0 +1,43 @@
+import { defineClientAppEnhance } from '@vuepress/client'
+import { FluentBundle } from '@fluent/bundle'
+import { createFluentVue } from 'fluent-vue'
+import PrimeVue from 'primevue/config'
+import Slider from 'primevue/slider'
+import Input from 'primevue/inputtext'
+import Dropdown from 'primevue/dropdown'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+
+import SimpleInput from '../components/SimpleInput.vue'
+import DateTime from '../components/DateTime.vue'
+import DateTimeCustom from '../components/DateTimeCustom.vue'
+
+// #region datefns
+import { format } from 'date-fns'
+
+const bundle = new FluentBundle('en', {
+  functions: {
+    DATEFNS (positionalArgs, namedArgs) {
+      const [date, formatString] = positionalArgs as [string, string]
+      return format(new Date(date), formatString)
+    }
+  }
+})
+
+const fluent = createFluentVue({
+  bundles: [bundle],
+})
+// #endregion datefns
+
+export default defineClientAppEnhance(({ app }) => {
+  app.use(fluent)
+  app.use(PrimeVue)
+  app.component('p-slider', Slider)
+  app.component('p-input', Input)
+  app.component('p-dropdown', Dropdown)
+  app.component('simple-input', SimpleInput)
+  app.component('date-time', DateTime)
+  app.component('date-time-custom', DateTimeCustom)
+})
---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,144 @@
+import type { DefaultThemeOptions } from 'vuepress'
+
+import { defineUserConfig } from 'vuepress'
+import { getHighlighter, Highlighter, BUNDLED_LANGUAGES } from 'shiki'
+import VueGrammar from 'shiki/languages/vue.tmLanguage.json'
+import FluentGrammar from './fluent.tmLanguage.json'
+
+let shikiHighlighter: Highlighter
+
+const shikiLanguages = BUNDLED_LANGUAGES
+  .filter(lang => lang.id !== 'vue')
+
+Object.assign(VueGrammar.repository, FluentGrammar.repository)
+
+// Add fluent support to shiki Vue lang definition
+VueGrammar.patterns.unshift(
+  {
+    "begin": "(<)(fluent)",
+    "beginCaptures": {
+      "1": {
+        "name": "punctuation.definition.tag.begin.html"
+      },
+      "2": {
+        "name": "entity.name.tag.style.html"
+      }
+    },
+    "end": "(</)(fluent)(>)",
+    "endCaptures": {
+      "1": {
+        "name": "punctuation.definition.tag.begin.html"
+      },
+      "2": {
+        "name": "entity.name.tag.style.html"
+      },
+      "3": {
+        "name": "punctuation.definition.tag.end.html"
+      }
+    },
+    "patterns": [
+      {
+          "begin": "(>)",
+          "end": "(?=</fluent>)",
+          "contentName": "source.ftl",
+          "patterns": [{
+            "include": "#comment"
+          },{
+            "include": "#message"
+          }]
+      }
+    ]
+  }
+)
+
+export default defineUserConfig<DefaultThemeOptions>({
+  title: 'fluent-vue',
+  description: 'Internationalization plugin for Vue.js. Vue.js integration for Project Fluent.',
+  themeConfig: {
+    repo: 'demivan/fluent-vue',
+    docsDir: 'docs',
+    docsBranch: 'develop',
+    editLinks: true,
+    smoothScroll: true,
+    displayAllHeaders: true,
+    sidebarDepth: 1,
+    sidebar: [
+      '/introduction.html',
+      '/instalation.html',
+      {
+        text: 'Api',
+        children: ['/api/instance-methods.html', '/api/v-t-directive.html', '/api/i18n-component.html', '/api/vue-i18n-comparison.html'],
+        collapsable: false,
+      },
+      {
+        text: 'HOWTO',
+        children: ['/howto/date-time.html', '/howto/access-outside-of-component.html'],
+        collapsable: false,
+      },
+      {
+        text: 'Integrations',
+        children: ['/integrations/webpack.html', '/integrations/vite.html'],
+        collapsable: false,
+      },
+    ]
+  },
+  plugins: [{
+    name: 'shiki',
+    async extendsMarkdown (md, app) {
+      if (!shikiHighlighter) {
+        shikiHighlighter = await getHighlighter({
+          theme: 'solarized-dark',
+          langs: [...shikiLanguages, {
+            id: 'vue',
+            scopeName: 'source.vue',
+            grammar: VueGrammar,
+            aliases: []
+          }, {
+            id: 'ftl',
+            scopeName: 'source.ftl',
+            grammar: FluentGrammar,
+            aliases: []
+          }
+        ]
+        })
+      }
+
+      function escapeHtml(html) {
+        const htmlEscapes = {
+          '&': '&amp;',
+          '<': '&lt;',
+          '>': '&gt;',
+          '"': '&quot;',
+          "'": '&#39;'
+        }
+
+        return html.replace(/[&<>"']/g, chr => htmlEscapes[chr])
+      }
+
+      md.options.highlight = ((code, lang) => {
+        if (!lang) {
+          return `<pre><code>${escapeHtml(code)}</code></pre>`
+        }
+
+        return shikiHighlighter.codeToHtml(code, lang)
+      })
+    }
+  }, '@vuepress/plugin-search'],
+  bundlerConfig: {
+    chainWebpack: (config) => {
+    config.module
+      .rule('fluent-vue')
+      .resourceQuery(/blockType=fluent/)
+      .use('fluent-vue')
+      .loader('fluent-vue-loader')
+
+    config.module
+      .rule('fix-fluent')
+      .include
+        .add((/@fluent[\\/](bundle|langneg|syntax|sequence)[\\/]/))
+        .end()
+      .test(/[.]js$/)
+      .type('javascript/esm')
+    }
+  }
+})
