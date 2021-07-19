22f6a3bbac2a73ee94ce192e2527547a01cb42a6
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-07-19 19:02:38 +0300

    Add more tests for webpack loader

--- packages/fluent-vue-loader/__tests__/test.spec.ts
+++ packages/fluent-vue-loader/__tests__/test.spec.ts
@@ -1,18 +1,31 @@
+import type { Stats } from 'webpack'
+
 import compiler from './compiler'
 
-test('Inserts name and outputs JavaScript', async () => {
-  // Arrange
-  // Act
-  const stats = await compiler('fixtures/test.vue')
+describe('webpack loader', () => {
+  test('generates custom block code', async () => {
+    // Arrange
+    // Act
+    const stats = await compiler('fixtures/test.vue')
 
-  // Assert
-  const ftlModules = stats.toJson({ source: true }).modules
-    ?.filter(module =>
-      module.name?.includes('blockType=fluent') === true &&
-      module.issuerName?.includes('blockType=fluent') === true)
-    .map(module => module.source)
+    // Assert
+    const ftlModules = stats.toJson({ source: true }).modules
+      ?.filter(module =>
+        module.name?.includes('blockType=fluent') === true &&
+        module.issuerName?.includes('blockType=fluent') === true)
+      .map(module => module.source)
 
-  expect(ftlModules).not.toBeUndefined()
-  expect(ftlModules).toHaveLength(1)
-  expect(ftlModules).toMatchSnapshot()
+    expect(ftlModules).not.toBeUndefined()
+    expect(ftlModules).toHaveLength(1)
+    expect(ftlModules).toMatchSnapshot()
+  })
+
+  test('errors with no locale attr', async () => {
+    // Arrange
+    const func = async (): Promise<Stats> => await compiler('fixtures/noLocale.vue')
+
+    // Act
+    // Assert
+    await expect(func).rejects.toMatchSnapshot()
+  })
 })
