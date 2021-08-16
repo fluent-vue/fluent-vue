9f8a495e0bd3c0821df0e07a4fc3a54d83636620
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-08-16 10:53:32 +0300

    Add failing test

---
+++ packages/fluent-vue/__tests__/performance.spec.ts
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { isReactive } from 'vue-demi'
+import { FluentBundle } from '@fluent/bundle'
+
+import { createFluentVue } from '../src'
+
+describe('performance checks', () => {
+  it('bundles are not deeply reactive', () => {
+    // Arrange
+    const bundle = new FluentBundle('en-US')
+
+    // Act
+    const fluent = createFluentVue({
+      bundles: [bundle]
+    })
+
+    // Assert
+    for (const bundle of fluent.bundles) {
+      expect(isReactive(bundle)).toEqual(false)
+    }
+  })
+})
