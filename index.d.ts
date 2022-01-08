ed02121c4265443f2b5b1e88ce09ede7c8d8eec1
Ivan Demchuk <ivan.demchuk@gmail.com>
2022-01-08 17:29:43 +0200

    Fix typescript errors with Vue 3

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,8 @@
-/* eslint-disable @typescript-eslint/prefer-ts-expect-error */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-ignore
-import Vue from 'vue'
-// @ts-ignore
-import '@vue/runtime-core'
-import { FluentResource } from '@fluent/bundle'
-
 // #region Vue 2
 declare module 'vue/types/vue' {
   interface Vue {
     $t: (key: string, values?: Record<string, unknown>) => string
     $ta: (key: string, values?: Record<string, unknown>) => Record<string, string>
-  }
-}
-
-declare module 'vue/types/options' {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  interface ComponentOptions<V extends Vue> {
-    /**
-     * Message override for Vue component
-     */
-    fluent?: { [locale: string]: FluentResource }
   }
 }
 // #endregion
@@ -30,12 +12,6 @@ declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     $t: (key: string, values?: Record<string, unknown>) => string
     $ta: (key: string, values?: Record<string, unknown>) => Record<string, string>
-  }
-}
-
-declare module '@vue/runtime-core' {
-  interface ComponentCustomOptions {
-    fluent?: { [locale: string]: FluentResource }
   }
 }
 // #endregion
