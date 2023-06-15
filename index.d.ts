44dc3eddb7e16db3d3afed6302aeeb87b310d5fb
Ivan Demchuk <ivan.demchuk@gmail.com>
2023-06-15 14:06:38 +0300

    Fix type definitions for `i18n` component and `v-t` directive (#822)

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,57 @@
-// #region Vue 2
-declare module 'vue/types/vue' {
-  interface Vue {
-    $t: (key: string, values?: Record<string, unknown>) => string
-    $ta: (key: string, values?: Record<string, unknown>) => Record<string, string>
-  }
-}
-// #endregion
+import { PropType, DefineComponent, FunctionDirective } from 'vue-demi'
+import { FluentVariable } from '@fluent/bundle'
 
-// #region Vue 3
-declare module '@vue/runtime-core' {
+type ComponentType = DefineComponent<{
+  /**
+   * The key of the translation.
+   */
+  path: {
+    type: StringConstructor,
+    required: true,
+  },
+  /**
+   * Arguments to pass to the translation.
+   */
+  args: {
+    type: PropType<Record<string, FluentVariable>>,
+    default: () => Record<string, FluentVariable>,
+  },
+  /**
+   * The tag to use as a root element.
+   */
+  tag: {
+    type: StringConstructor,
+    default: 'span',
+  },
+  /**
+   * Whether to render translation as html.
+   */
+  html: {
+    type: BooleanConstructor,
+    default: false,
+  },
+  /**
+   * Whether to render translation without a root element.
+   */
+  noTag: {
+    type: BooleanConstructor,
+    default: false,
+  }
+}>
+
+declare module 'vue' {
   interface ComponentCustomProperties {
     $t: (key: string, values?: Record<string, unknown>) => string
     $ta: (key: string, values?: Record<string, unknown>) => Record<string, string>
   }
+
+  interface ComponentCustomProperties {
+    vT: FunctionDirective<HTMLElement, Record<string, FluentVariable>>
+  }
+
+  interface GlobalComponents {
+    i18n: ComponentType
+  }
 }
-// #endregion
 
 export * from './dist'
