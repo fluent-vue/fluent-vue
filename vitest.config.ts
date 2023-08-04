59020f7979f709d451b71fc00eae3d4b117a50af
Ivan Demchuk <ivan.demchuk@gmail.com>
2023-08-04 09:08:41 +0000

    Fix coverage not working

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': '\'test\'',
+  },
   test: {
     include: [
       '__tests__/**/*.spec.ts',
     ],
+    experimentalVmThreads: true,
     coverage: {
       provider: 'istanbul',
       reporter: ['text', 'lcovonly'],
