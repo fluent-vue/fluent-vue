8862f481a3dfc0b529d4a444d28f33ab686336a2
Ivan Demchuk <ivan.demchuk@gmail.com>
2023-08-05 09:19:44 +0000

    Update example build script

--- scripts/build-examples.mjs
+++ scripts/build-examples.mjs
@@ -1,27 +1,38 @@
-import { readdirSync, readFileSync, existsSync } from 'fs'
-import { execa } from 'execa'
+import { existsSync, readFileSync, readdirSync } from 'node:fs'
+import { spawn } from 'node:child_process'
+
+function exec(command, params, ops) {
+  const p = spawn(command, params, ops)
+
+  return new Promise((resolve) => {
+    p.on('exit', (code) => {
+      resolve(code)
+    })
+  })
+}
 
 const examples = readdirSync('examples')
-  .filter((f) => existsSync(`examples/${f}/package.json`))
-  .map((f) => ({
+  .filter(f => existsSync(`examples/${f}/package.json`))
+  .map(f => ({
     folder: `examples/${f}`,
     package: JSON.parse(readFileSync(`examples/${f}/package.json`)),
   }))
 
-async function buildExamples () {
-  await execa('pnpm', ['i'], { stdio: 'inherit' })
-  await execa('pnpm', ['build'], { stdio: 'inherit' })
+async function buildExamples() {
+  await exec('pnpm', ['i'], { stdio: 'inherit' })
+  await exec('pnpm', ['build'], { stdio: 'inherit' })
+  await exec('pnpm', ['rimraf', 'node_modules'], { stdio: 'inherit' })
 
   for (const example of examples) {
     console.log(`building ${example.folder}...`)
-    await execa('pnpm', ['add', `file:../../`], { stdio: 'inherit', cwd: example.folder })
-    await execa('pnpm', ['i'], { stdio: 'inherit', cwd: example.folder })
-    await execa('pnpm', ['build'], { stdio: 'inherit', cwd: example.folder })
+    await exec('pnpm', ['add', 'file:../../'], { stdio: 'inherit', cwd: example.folder })
+    await exec('pnpm', ['i'], { stdio: 'inherit', cwd: example.folder })
+    await exec('pnpm', ['build'], { stdio: 'inherit', cwd: example.folder })
   }
 
-  for (const typescriptExample of examples.filter((ex) => ex.package.scripts.typecheck != null)) {
+  for (const typescriptExample of examples.filter(ex => ex.package.scripts.typecheck != null)) {
     console.log(`typechecking ${typescriptExample.folder}...`)
-    await execa('pnpm', ['typecheck'], { stdio: 'inherit', cwd: typescriptExample.folder })
+    await exec('pnpm', ['typecheck'], { stdio: 'inherit', cwd: typescriptExample.folder })
   }
 }
 
