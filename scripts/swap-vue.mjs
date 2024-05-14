24a69697b0960a086c63aab1c0b2a1e00fe755b0
Ivan Demchuk <ivan.demchuk@gmail.com>
2024-05-14 17:51:14 +0300

    chore: update Vue dependencies in test scripts

--- scripts/swap-vue.mjs
+++ scripts/swap-vue.mjs
@@ -3,11 +3,11 @@ import process from 'node:process'
 import { execa } from 'execa'
 
 const vue3packages = {
-  'vue': 'npm:vue@^3.4.21',
+  'vue': 'npm:vue@^3.4.27',
   'vue-2': 'npm:vue@^2.7.16',
-  'vue-3': 'npm:vue@^3.4.21',
-  '@vue/compiler-sfc': '^3.4.21',
-  '@vue/test-utils': '^2.4.5',
+  'vue-3': 'npm:vue@^3.4.27',
+  '@vue/compiler-sfc': '^3.4.27',
+  '@vue/test-utils': '^2.4.6',
 }
 
 const vue2packages = {
