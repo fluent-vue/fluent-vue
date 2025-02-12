0751b5173cbf968b0138ec778bde5e1151ff5d14
Ivan Demchuk <ivan.demchuk@gmail.com>
2025-02-12 16:09:40 +0200

    Add display for missing translations

--- src/devtools/plugin.ts
+++ src/devtools/plugin.ts
@@ -1,14 +1,29 @@
 import type { FluentBundle } from '@fluent/bundle'
 
 import type { FluentVue } from 'src'
-import type { App } from 'vue-demi'
+import type { App, ComponentInternalInstance } from 'vue-demi'
 import type { ResolvedOptions } from '../types'
 import { setupDevToolsPlugin } from '@vue/devtools-api'
-import { watchEffect } from 'vue-demi'
+import { getCurrentInstance, watchEffect } from 'vue-demi'
 import pseudoLocalize from './pseudoLocalize'
 
 export function registerFluentVueDevtools(app: App, options: ResolvedOptions, fluent: FluentVue) {
   let currentPseudoLocalize: ((str: string) => string) | undefined
+  const missingTranslations: WeakMap<ComponentInternalInstance, Set<string>> = new WeakMap()
+
+  // Hook into options and app
+  const oldWarnMissing = options.warnMissing
+  options.warnMissing = (key) => {
+    const instance = getCurrentInstance()
+
+    oldWarnMissing(key)
+
+    if (!instance)
+      return
+
+    missingTranslations.set(instance, missingTranslations.get(instance) ?? new Set())
+    missingTranslations.get(instance)!.add(key)
+  }
 
   // Hook into bundle tranform
   const hookedBundles = new WeakSet<FluentBundle>()
@@ -101,6 +116,37 @@ export function registerFluentVueDevtools(app: App, options: ResolvedOptions, fl
           label: 'localized',
           textColor: 0x000000,
           backgroundColor: 0x41B883,
+        })
+      }
+
+      const missing = missingTranslations.get(componentInstance)
+      if (missing != null && missing.size > 0) {
+        treeNode.tags.push({
+          label: 'missing translations',
+          textColor: 0xFFFFFF,
+          backgroundColor: 0xB00020,
+        })
+      }
+    })
+
+    api.on.inspectComponent(({ componentInstance, instanceData }) => {
+      if (!componentInstance?.proxy?.$options.fluent)
+        return
+
+      const missing = missingTranslations.get(componentInstance)
+
+      if (missing) {
+        instanceData.state.push({
+          type: 'fluent-vue',
+          key: 'missingTranslations',
+          editable: false,
+          value: {
+            _custom: {
+              type: 'set',
+              value: [...missing.values()],
+              display: JSON.stringify([...missing.values()]),
+            },
+          },
         })
       }
     })
