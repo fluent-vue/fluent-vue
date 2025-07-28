2fd1f8a545994a0c2e3750ff4dfaf6e4ca777fe3
Ivan Demchuk <ivan.demchuk@gmail.com>
2025-07-28 22:36:09 +0300

    Prevent extra Vue warning

--- src/composition.ts
+++ src/composition.ts
@@ -9,7 +9,7 @@ export function useFluent(): TranslationContext {
   const instance = getCurrentInstance()
   assert(instance != null, 'useFluent called outside of setup')
 
-  const rootContext = inject(RootContextSymbol)
+  const rootContext = inject(RootContextSymbol, undefined)
   assert(rootContext != null, 'useFluent called without installing plugin')
 
   return getContext(rootContext, instance.proxy, true)
